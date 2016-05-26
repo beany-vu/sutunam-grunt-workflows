078a0d0f68cc4afce3c1d7f403970f2f284598fd
Vu Quang Hoang <vuquanghoang@sutunam.com>
2016-05-26 10:26:40 +0700

    update path to watch scss file. Now it can watch to files in sub-folders (recursively) instead of only scss files in the given folder

--- gruntfile.js
+++ gruntfile.js
@@ -25,7 +25,7 @@ module.exports = function(grunt) {
         },
         watch: {
             css: {
-                files: 'scss/*.scss',
+                files: 'scss/**/*.scss',
                 tasks: ['sass', 'postcss']
             }
         }
